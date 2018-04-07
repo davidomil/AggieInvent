1ee3261daaf093f6e2adf15d17f1263db7f93c8a
davidomil <david.miler@atlas.cz>
2018-04-07 14:43:03 -0500

    Added events and Serial class

---
+++ Serial.py
@@ -0,0 +1,37 @@
+import threading
+
+
+class DoorSerial(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            pass
+
+    def build_message(self,command,param1 = None, param2 = None,data = None):
+        """
+        Builds message to send over RS232
+        Packet structure:
+        |----------|----------------------------------|--------------|
+        |  HEADER  |             DATA                 |Data Check Sum|
+        |----------|----------------------------------|--------------|
+             24                Data Size                       4
+        ^          ^
+        |             \
+        |                \
+        |     Packet HEADER  \
+        |------------|---------|---------|---------|-----------|------------|------------------|
+        |Start Byte  | Command | Param 1 | Param 2 | Data Size | Error Code | Header Check Sum |
+        |------------|---------|---------|---------|-----------|------------|------------------|
+              1           4          4        4          4            4              4
+
+        :param command: Command to send
+        :param param1: Command 1st param
+        :param param2:  Command 2nd param
+        :param data: Data
+        :return:
+        """
+
+        raise NotImplemented
+
